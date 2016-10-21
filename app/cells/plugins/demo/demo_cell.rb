f383f3969b8aea43cd9236745b2579496561743f
ElliottAYoung <elliott.a.young@gmail.com>
2016-10-21 11:22:31 -0500

    Change method to take data input

--- app/cells/plugins/demo/demo_cell.rb
+++ app/cells/plugins/demo/demo_cell.rb
@@ -1,8 +1,14 @@
 module Plugins
   module Demo
     class DemoCell < Plugins::Demo::Cell
-      def show
+      def marquee
         render
+      end
+
+      private
+
+      def field_data
+        @model.data.values.join(" ")
       end
     end
   end
